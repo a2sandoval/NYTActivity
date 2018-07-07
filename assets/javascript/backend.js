7565d8bfa5b979e4e7d66bd2763da9dcd731bb31
larry.moua <lkmoua@gmail.com>
2018-07-07 13:56:23 -0700

    Phase 1

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -1,11 +1,33 @@
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" +
-         "&api_key=963660807a3c404f8d3159459af6d1a1=&limit=10";
+// var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json" +
+//          "&api_key=963660807a3c404f8d3159459af6d1a1=&limit=10";
+// $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//       }).then(function(JSON) {
+
+//           console.log(JSON);
+
+//     var copyRight = JSON.copyright;
+//     var 
+
+//     var searchQ = $();
+// var begDate = $();
+// var endDate = $();
+
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "963660807a3c404f8d3159459af6d1a1",
+  'q': searchQ;
+  'begin_date': begDate;
+  'end_date': endDate;
+});
 $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(JSON) {
-
-          console.log(JSON);
-
-    var results = JSON.data;
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
